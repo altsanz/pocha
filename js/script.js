d28a27f10c5bdb76c9cbcbb37c8321719f4b420c
noyss <asanz91@gmail.com>
2013-06-05 03:42:57 +0200

    Working adding players on the list

--- js/script.js
+++ js/script.js
@@ -36,5 +36,39 @@ $(function(){
 		model: player
 	});
 
-	
+	/* The application. */
+
+	var AppView = Backbone.View.extend({
+		el: $('#initPlayers'),
+		events: {
+			"keypress #new-player" : "createOnEnter"
+		},
+		initialize : function() {
+			this.input = this.$('#new-player');
+
+			this.listenTo(Players, 'add', this.addOne);
+			this.listenTo(Players, 'reset', this.addAll);
+			this.listenTo(Players, 'all', this.render);
+
+			Players.fetch();
+		},
+		addOne : function(player) {
+			var view = new PlayerView({model: player});
+			view.render();
+			this.$("#listPlayers").append(view.el);
+		},
+		addAll: function() {
+			Players.each(this.addOne, this);
+		},
+		createOnEnter : function(e) {
+			if (e.keyCode != 13) return;	// Detect if not enter was pressed
+			if(!this.input.val()) return;	// Detect if input value is empty
+
+			Players.create({name: this.input.val()});
+			this.input.val('');
+		}
+	});
+
+	var App = new AppView;
+
 });
