1e94ee43bd91d36bde4f8559d59fa5860cfb7380
noyss <asanz91@gmail.com>
2013-06-05 03:03:29 +0200

    Added localStorage.Backbone adapter and developed PlayerView

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
-var Player = Backbone.Model.extends({});
+$(function(){
+var Player = Backbone.Model.extends({
+	defaults : function() {
+		return {
+			name: 'DefaultPlayer'
+		};
+	}
+});
 
-var PlayerView = Backbone.View.extends({});
+var player = new Player({
+	name: 'Default player'
+});
+
+var PlayerView = Backbone.View.extends({
+	render : function(){
+		var html = '<li>'+this.model.get('name')+'<img src="./img/minus-alt.png" alt="Eliminar jugador">';
+		this.$el.html(html);
+	}
+});
+
+var playerView = new PlayerView({
+	model: player
+});
+});
