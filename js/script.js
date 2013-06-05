c0e02233c896168af41e7b4ac3887533e037f8ea
noyss <asanz91@gmail.com>
2013-06-05 04:00:54 +0200

    Names are now removable!

--- js/script.js
+++ js/script.js
@@ -21,7 +21,11 @@ $(function(){
 	var PlayerView = Backbone.View.extend({
 		template: _.template('<li><%= name %><img class="deletePlayer" src="./img/minus-alt.png" alt="Eliminar jugador"></li>'),
 		events : {
-			"click .deletePlayer" : "clear"
+			"click img.deletePlayer" : "clear"
+		},
+		initialize : function() {
+			this.listenTo(this.model, 'change', this.render);
+			this.listenTo(this.model, 'destroy', this.remove);
 		},
 		render : function(){
 			var attributes = this.model.toJSON();
