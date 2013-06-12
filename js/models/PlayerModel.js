8c6aaa679601f3b6418216c63e6139f34986c189
altsanz <asanz91@gmail.com>
2013-06-12 22:31:50 +0200

    PlayerModel and PlayerCollection created

---
+++ js/models/PlayerModel.js
@@ -0,0 +1,13 @@
+app.models.Player = Backbone.Model.extend({
+	defaults: function() {
+		return {
+			name: 'DefaultPlayer'
+		}
+	}
+});
+
+app.models.PlayerCollection = Backbone.Collection.extend({
+		model: app.models.Player,
+		localStorage: new Backbone.LocalStorage('players-backbone')
+});
+
